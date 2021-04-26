2df03e8cd27e41351769e6fb65a44c5d98212f80
danielsoftdeveloper <daniel.andrade@sga.pucminas.br>
2021-04-25 21:26:25 -0300

    Trabalhando com datas em Javascript.

--- js/main.js
+++ js/main.js
@@ -108,7 +108,18 @@ while(count < 5){
 
 
 // aula 16 estrutura de repetição for.
-var count;
+/* var count;
 for( count = 0; count < 5; count++){
     alert(count);
-}
+} */
+
+
+//aula 17 trabalhando com Datas
+var d = new Date();
+//alert(d);
+//alert(d.getMonth()+1);
+//alert(d.getMinutes());
+
+console.log(d.getHours());
+console.log(d.getMinutes());
+console.log(d.getSeconds());
